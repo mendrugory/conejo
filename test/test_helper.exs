8a7371a5be3bfeaab62ca18a6a487f1fda920c5f
gonzalo <gj@palmtreestatistics.com>
2017-01-04 17:53:08 +0100

    Running RabbitMQ Docker Container from the tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -7,10 +7,10 @@ cmds =
     {"docker", ["run", "-d", "-p", "15672:15672", "-p", "5672:5672", "--name", "rabbitmq", "rabbitmq:3-management"]}
   ]
 
-#Enum.each(cmds, fn {cmd, args} -> System.cmd(cmd, args) end)
+Enum.each(cmds, fn {cmd, args} -> System.cmd(cmd, args) end)
 
 Logger.info "Waiting for RabbitMQ Broker (Docker Container) ..."
-#Process.sleep(10000)
+Process.sleep(10000)
 
 Application.ensure_all_started(:conejo)
 Logger.info "Conejo Tests begin ..."
