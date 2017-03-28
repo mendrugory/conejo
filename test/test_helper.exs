bcadd94ec1d940da2d046a5bc979b002e0bd55e9
gonzalo <gj@palmtreestatistics.com>
2017-03-28 12:45:44 +0200

    adapting tests to travis

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,16 +1,18 @@
 require Logger
 
-cmds =
-  [
-    {"docker", ["kill", "rabbitmq"]},
-    {"docker", ["rm", "rabbitmq"]},
-    {"docker", ["run", "-d", "-p", "15672:15672", "-p", "5672:5672", "--name", "rabbitmq", "rabbitmq:3-management"]}
-  ]
+unless System.get_env "TRAVISCICONEJO" do
+  cmds =
+    [
+      {"docker", ["kill", "rabbitmq"]},
+      {"docker", ["rm", "rabbitmq"]},
+      {"docker", ["run", "-d", "-p", "15672:15672", "-p", "5672:5672", "--name", "rabbitmq", "rabbitmq:3-management"]}
+    ]
 
-Enum.each(cmds, fn {cmd, args} -> System.cmd(cmd, args) end)
+  Enum.each(cmds, fn {cmd, args} -> System.cmd(cmd, args) end)
 
-Logger.info "Waiting for RabbitMQ Broker (Docker Container) ..."
-Process.sleep(5_000)
+  Logger.info "Waiting for RabbitMQ Broker (Docker Container) ..."
+  Process.sleep(5_000)
+end
 
 Application.ensure_all_started(:conejo)
 Logger.info "Conejo Tests begin ..."
