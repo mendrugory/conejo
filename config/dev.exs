49ce9dc6094be8b6fb48a10d80c5b99d78aca3f7
gonzalo <gj@palmtreestatistics.com>
2017-03-28 12:24:58 +0200

    adding dev.exs

--- config/dev.exs
+++ config/dev.exs
@@ -1,10 +1,21 @@
 use Mix.Config
 
-import_config "dev.secret.exs"
+config :conejo,
+ host: "0.0.0.0",
+ port: 5672,
+ username: "guest",
+ password: "guest"
+
+config :conejo, :consumer,
+ exchange: "amq.topic",
+ exchange_type: "topic",
+ queue_name: "my_queue",
+ queue_declaration_options: [{:auto_delete, true}, {:exclusive, true}],
+ queue_bind_options: [routing_key: "example"],
+ consume_options: [no_ack: true]
 
 
-
-
-
-
+config :conejo, :publisher,
+ exchange: "amq.topic",
+ exchange_type: "topic"
 
