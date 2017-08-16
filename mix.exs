282d5f78e515e27081a166021e2f37e3abc7296e
gonzalo <gj@palmtreestatistics.com>
2017-08-16 17:22:07 +0200

    Elixir 1.5 v0.3.3

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Conejo.Mixfile do
   def project do
     [app: :conejo,
      version: @version,
-     elixir: "~> 1.4",
+     elixir: "~> 1.5",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      description: description(),
